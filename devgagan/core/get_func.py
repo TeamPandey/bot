6373f48885bb8527a6a445dcc5657d4fcbebea3f
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-03 00:07:34 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -453,7 +453,7 @@ async def copy_message_with_chat_id(app, userbot, sender, chat_id, message_id, e
                 await edit.edit("Unsupported media type.")
 
     except Exception as e:
-        print(f"Error : {e}"}
+        print(f"Error : {e}")
         pass
         #error_message = f"Error occurred while processing message: {str(e)}"
         # await app.send_message(sender, error_message)
