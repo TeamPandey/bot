d54ed728d7c68bbe4ac5b7852d94a437d179cc05
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-04 16:10:05 +0530

    Update eval.py

--- devgagan/modules/eval.py
+++ devgagan/modules/eval.py
@@ -1,15 +1,4 @@
-# ---------------------------------------------------
-# File Name: eval.py 
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Unknown
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
+
 # ---------------------------------------------------
 
 import os, re, subprocess, sys, traceback
@@ -42,7 +31,7 @@ async def edit_or_reply(msg, **kwargs):
     & ~filters.via_bot
 )
 @app.on_message(
-    filters.command(["evv", "evr"])
+    filters.command(["eval", "val"])
     & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
@@ -148,13 +137,13 @@ async def forceclose_command(_, CallbackQuery):
 
 
 @app.on_edited_message(
-    filters.command("shll")
+    filters.command("sh")
     & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
 @app.on_message(
-    filters.command("shll")
+    filters.command("sh")
     & filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
